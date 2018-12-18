42aee393c11fe3d8eebb7d8e8239d1c5cd6089a8
liulonghaohao <45375735+liulonghaohao@users.noreply.github.com>
2018-12-18 09:50:15 +0800

    课堂编写

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,1 +1,15 @@
-
+  int []a = {5,1,2,4,3};//11
+  int b = a.length;
+  while(b!=0){
+  for(int i = 0;i<a.length-1;i++){
+      if(a[i]>a[i+1]){
+         int temp ="
+         a[i] = a[i+1];
+         a[i+1] = a[i];
+      }
+      b--;
+      
+    }
+      
+           
+      
